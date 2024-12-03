0660138616a090404f51bdaa928ef8c19043434d
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-12-03 10:18:21 +0200

    installed semantic reactand added table

--- app/Cards.jsx
+++ app/Cards.jsx
@@ -1,9 +1,48 @@
 import { useState, useEffect } from "react";
-import { SafeAreaView } from "react-native-web";
 import { SafeAreaView } from "react-native-safe-area-context";
+import 'semantic-ui-css/semantic.min.css';
+import axios from 'axios';
+// import {
+//     TableRow,
+//     TableHeaderCell,
+//     TableHeader,
+//     TableFooter,
+//     TableCell,
+//     TableBody,
+//     Table,
+//   } from 'semantic-ui-react'
+  
 
 export default function Cards(){
     return(
-        <SafeAreaView></SafeAreaView>
+        <SafeAreaView>
+            <Table size='small'>
+    <TableHeader>
+      <TableRow>
+        <TableHeaderCell>Name</TableHeaderCell>
+        <TableHeaderCell>Status</TableHeaderCell>
+        <TableHeaderCell>Notes</TableHeaderCell>
+      </TableRow>
+    </TableHeader>
+
+    <TableBody>
+      <TableRow>
+        <TableCell>John</TableCell>
+        <TableCell>Approved</TableCell>
+        <TableCell>None</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell>Jamie</TableCell>
+        <TableCell>Approved</TableCell>
+        <TableCell>Requires call</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell>Jill</TableCell>
+        <TableCell>Denied</TableCell>
+        <TableCell>None</TableCell>
+      </TableRow>
+    </TableBody>
+  </Table>
+        </SafeAreaView>
     )
 }
